8da46b4cdaea0a766fc568e6e5cef45107b23286
sachinsipl <sachin.gupta@systematixindia.com>
2014-06-20 11:05:53 +0530

    Update program2.cpp

--- program2/program2.cpp
+++ program2/program2.cpp
@@ -1,11 +1,13 @@
 
+/*Write code to reverse a C-Style String (C-String means that “abcd” is represented as 
+five characters, including the null character )*/
 #include <iostream>
 using namespace std;
 
 class reverseStr {
 
 public:
-
+// function for reverse string 
 void reverseString(char *str)
 {
    if(*str) {
