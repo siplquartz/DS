2d49948944d444855afc81cdf88e06bc9361fbca
sachinsipl <sachin.gupta@systematixindia.com>
2014-06-21 17:15:35 +0530

    Update program1.cpp

--- program1/program1.cpp
+++ program1/program1.cpp
@@ -1,14 +1,14 @@
 // program 1.Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
 #include <iostream>
 using namespace std;
-class checkUniqe{
+class checkUnique{
 
  private:
   string str;
 
  public:
   // constructor function to initialize string  
-   checkUniqe(string charr){
+   checkUnique(string charr){
       str = charr;
    }
  
@@ -28,7 +28,7 @@ class checkUniqe{
 int main()
 {
   // Create object with string parameter
-  checkUniqe obj("complexity"); 
+  checkUnique obj("complexity"); 
   bool result =  obj.check(); 
   if(result)
    cout << "String has unique characters.";
