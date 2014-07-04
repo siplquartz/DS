0d7a3efdf5da2547018fc31a014e998fdf70f966
sachinsipl <sachin.gupta@systematixindia.com>
2014-07-04 16:59:23 +0530

    Create reverse_string.cpp
    updated program

---
+++ updated-program/reverse_string.cpp
@@ -0,0 +1,38 @@
+/*Write code to reverse a C-Style String (C-String means that “abcd” is represented as 
+five characters, including the null character )*/
+#include <iostream>
+using namespace std;
+
+class reverseStr {
+
+	public:
+	// function for reverse string 
+	string reverse (string str) { 
+		string tempStr="";
+
+		for (int i=0; i<str.length( ) ; i++) 
+		    tempStr = str[ i ] + tempStr ; 
+		
+		return tempStr; 
+	}
+}; 
+
+int main() {
+      string inputString;  
+      cout<<"Program to reverse a string\n";
+  
+      cout<<"Please provide string:\n";
+      getline (cin,inputString);
+      reverseStr obj;
+    
+      if(inputString.length()<2){
+        cout<<"Provided string must content more than two characters\n";
+        std::exit(EXIT_FAILURE);
+      }
+
+
+      cout << obj.reverse(inputString);
+
+     return 0;     
+ } 
+
