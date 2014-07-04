4f8907eb1692a5e07ddc94e0eae09c8e1276c0a6
sachinsipl <sachin.gupta@systematixindia.com>
2014-07-04 17:55:21 +0530

    Create remove_duplicate_char.cpp
    updated program

---
+++ updated-program/remove_duplicate_char.cpp
@@ -0,0 +1,59 @@
+/*
+ 4.Design an algorithm and write code to remove the duplicate characters in a string 
+without using any additional buffer NOTE: One or two additional variables are fine 
+An extra copy of the array is not 
+FOLLOW UP
+Write the test cases for this method 
+*/
+#include <iostream>
+using namespace std;
+class removeDuplicateChar {
+   public:
+        // function for remove duplicates characters
+        char * removeDuplicates(char* str) {
+             int index = 1;
+             for(int i = 1; str[i]!='\0'; i++) {
+                  int j ;
+                for(j= 0; j < index; j++) {
+                    if(str[i] == str[j]) break;
+                }
+                
+                if(j == index) {
+                    str[index] = str[i];
+                    index++;
+                }
+            }
+         str[index] = '\0';
+         return str;
+        }
+};
+int main(){
+     removeDuplicateChar obj;
+     char * stringArray = (char *) malloc (sizeof (char)*100); 
+     
+     cout<<"Please provide string:\n";
+     cin>>stringArray;
+     
+     if(sizeof(stringArray)>1) {
+        char  *str1 = obj.removeDuplicates(stringArray);
+        cout << str1;
+     }else{
+         cout<<"Provided string must content more than two characters\n";
+         std::exit(EXIT_FAILURE);
+     }
+   return 0;
+}
+
+/*
+Test Cases
+(1) input string  -: "saaaassss"
+    output -:  "sa"
+  
+(2) input string  -: "s@@chinsa"
+    output -:  "s@chin"
+
+
+(3) input string  -: "sachinsachin"
+    output -:  "sachin"
+
+*/
