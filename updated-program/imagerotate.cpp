38e520988a2999376b1802694f801e49016a8303
sachinsipl <sachin.gupta@systematixindia.com>
2014-07-11 16:04:29 +0530

    Create imagerotate.cpp

---
+++ updated-program/imagerotate.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int size; 
+  cout<<"\n Please provide image matrix size in number ";
+
+  if(cin>>size){
+
+       int** image = new int*[size];
+       int** tempImage = new int*[size];
+       int count= 1;
+       
+       for (int i = 0; i < size; i++)
+         image[i]= new int[size];
+   
+       for (int i = 0; i < size; i++)
+         tempImage[i]= new int[size];
+   
+       bool inputFlag = true;
+
+       // Fill the matrix image
+       
+
+        for (int i = 0; i < size; ++i) {
+            
+            for (int j = 0; j <size; ++j) {
+              
+                cout<<"\nEnter the value for image position ["<<i<<"]["<<j<<"] ";
+                
+                if(cin>>image[i][j]) {
+                 } else {
+                  inputFlag=false;
+                  break;
+                 }
+            }
+            
+            if(!inputFlag)
+              break;
+      
+        }
+
+        if(inputFlag){
+            cout<<"\n The provided image is\n";
+            for (int i = 0; i < size; ++i) {
+              for (int j = 0; j < size; ++j)  {
+                cout<<image[i][j];
+                cout<<"  ";
+              }
+              cout<<"\n";
+            }
+
+          // rotating the image 
+          for (int i = 0; i < size; ++i) {
+            for (int j = 0; j <size; ++j) {
+              tempImage[i][j]=image[(size-1)-j][i];
+            }
+          }
+          
+          cout<<"\n ************ Rotated image ********************** \n";
+          for (int i =0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+              cout<<tempImage[i][j];
+              cout<<" ";
+            }
+            cout<<"\n";
+          }
+
+        }else 
+         cout<<"\nPlease enter only integers  numbers."; 
+
+
+   }else 
+    cout<<"\nPlease enter only integers  numbers."; 
+
+
+ return 0;
+}
