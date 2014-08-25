c82a8be8c8f9a6e9fa8fbc040618160ce62ac331
sachinsipl <sachin.gupta@systematixindia.com>
2014-08-25 16:17:48 +0530

    Update program4.cpp

--- assignment3/program4.cpp
+++ assignment3/program4.cpp
@@ -1,3 +1,18 @@
+/*
+
+MS Excel columns has a pattern like A, B, C, … ,Z, AA, AB, AC,…. ,AZ, BA, BB, … ZZ, AAA, AAB ….. etc. In other words, column 1 is named as “A”, column 2 as “B”, column 27 as “AA”.
+Given a column number, find its corresponding Excel column name. Following are more examples.
+Input          Output
+ 26             Z
+ 51             AY
+ 52             AZ
+ 80             CB
+ 676            YZ
+ 702            ZZ
+ 705            AAC
+*/
+ 
+ 
  #include<iostream>
  using namespace std;
  class MSExcel {
